2a5d60c33277805ffd7cfc747e7b8ec80ca937b4
Ruben Caceiro <caceiro.ruben@gmail.com>
2019-02-09 17:22:38 +0000

    get new changes

--- src/main.c
+++ src/main.c
@@ -2,10 +2,13 @@
 
 int main()
 {
- yolo_object *yolo=NULL;
- yolo_detection_image *detection_image=NULL;
+ void *yolo=NULL;
+ image_detection *detection_image=NULL;
+ video_detection *detection_video=NULL;
  yolo_init(&yolo, "./darknet", "./cfg/coco.data", "./cfg/yolov3.cfg", "../weights/yolov3.weights");
  yolo_detect_image(yolo, &detection_image, "./darknet/data/kite.jpg", 0.5);
- yolo_detection_image_free(&detection_image);
- yolo_cleanup(yolo);
+ yolo_detect_video(yolo, &detection_video, "./data/test.mp4", 0.5, 1);
+ image_detection_free(&detection_image);
+ video_detection_free(&detection_video);
+ yolo_free(yolo);
 }
